c4e223ef1ec08ec4e490a321eb57b57e393749d6
Abhay Nikam <nikam.abhay1@gmail.com>
2020-10-26 19:18:05 +0530

    Fix the sample app for test to use rails 6.0 instead of 6.1-alpha

--- tmp/templates/app_template/config/application.rb
+++ tmp/templates/app_template/config/application.rb
@@ -9,7 +9,7 @@ require "rails/all"
 module AppTemplate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
