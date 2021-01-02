eca86cc76f6cd1aac7f009047a22128c0ec6fc17
Abhay Nikam <nikam.abhay1@gmail.com>
2021-01-02 13:01:02 +0530

    Fixes the option description display format [ci skip]

--- lib/generators/boring/ci/github_action/install/install_generator.rb
+++ lib/generators/boring/ci/github_action/install/install_generator.rb
@@ -14,9 +14,7 @@ module Boring
         DEFAULT_REPOSITORY_NAME = "boring_generators"
 
         class_option :ruby_version,     type: :string, aliases: "-v",
-                                                       desc: "Tell us the ruby version which you use for the application. Default to Ruby #{DEFAULT_RUBY_VERSION},
-                                                       which will cause the action to use the version specified
-                                                       in the #{RUBY_VERSION_FILE} file."
+                                                       desc: "Tell us the ruby version which you use for the application. Default to Ruby #{DEFAULT_RUBY_VERSION}, which will cause the action to use the version specified in the #{RUBY_VERSION_FILE} file."
         class_option :node_version,     type: :string, aliases: "-v",
                                                        desc: "Tell us the node version which you use for the application. Default to Node #{DEFAULT_NODE_VERSION}"
         class_option :repository_name,  type: :string, aliases: "-rn",
