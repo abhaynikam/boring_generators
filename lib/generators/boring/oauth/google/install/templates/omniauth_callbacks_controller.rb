265ec04608ccac0ad608c6cc4dbaf9aac8befb9d
Abhay Nikam <nikam.abhay1@gmail.com>
2021-02-06 02:05:03 +0530

    Fixes CI

--- lib/generators/boring/oauth/google/install/templates/omniauth_callbacks_controller.rb
+++ lib/generators/boring/oauth/google/install/templates/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # See https://github.com/omniauth/omniauth/wiki/FAQ#rails-session-is-clobbered-after-callback-on-developer-strategy
-  skip_before_action :verify_authenticity_token, only: :github
+  skip_before_action :verify_authenticity_token, only: :google_auth
 
   def google_auth
     # You need to implement the method below in your model (e.g. app/models/user.rb)
