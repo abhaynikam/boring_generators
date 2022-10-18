35e90d19ee76913deb67860afd0ab0597857855b
jackparsons93 <jackparsons93@gmail.com>
2022-10-17 20:53:29 -0500

    Still having issues with add_turbo_stream
    retrying again with binding.pry to check to see if :add_turbo is true or false when option is passed in

--- lib/generators/boring/devise/install/install_generator.rb
+++ lib/generators/boring/devise/install/install_generator.rb
@@ -67,7 +67,9 @@ module Boring
         end
       end
       def add_turbo_stream
-        return if !options[:add_turbo]
+        binding.pry
+        return if options[:add_turbo] == false
+         
           insert_into_file "config/initializers/devise.rb", <<~RUBY, after: /config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'/
             \n
             \tconfig.navigational_formats = ['*/*', :html, :turbo_stream]
