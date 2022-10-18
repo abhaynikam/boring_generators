2d43a19a27cf4987b34c07b8ae6c725f2e589c9d
jackparsons93 <jackparsons93@gmail.com>
2022-10-17 22:01:09 -0500

    forgot to bundle
    I was making an error while troubleshooting forgetting to rerun bundler

--- lib/generators/boring/devise/install/install_generator.rb
+++ lib/generators/boring/devise/install/install_generator.rb
@@ -70,14 +70,14 @@ module Boring
         #puts options[:add_turbo]
         #puts "hello world"
         say "Hello world" , :green
-        #say " options #{options[:add_turbo]} " , :green 
-       # return if options[:add_turbo] == false 
-       #  
-       #   insert_into_file "config/initializers/devise.rb", <<~RUBY, after: /config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'/
-       #     \n
-       #     \tconfig.navigational_formats = ['*/*', :html, :turbo_stream]
-       # RUBY
-       # end
+        say " options #{options[:add_turbo]} " , :green 
+        return if options[:add_turbo] == false 
+         
+          insert_into_file "config/initializers/devise.rb", <<~RUBY, after: /config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'/
+            \n
+            \tconfig.navigational_formats = ['*/*', :html, :turbo_stream]
+          RUBY
+        end
       end
     end
   end
