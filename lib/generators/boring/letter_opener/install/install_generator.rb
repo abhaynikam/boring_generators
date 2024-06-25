21888e76add1c326697167a68ee7b1e3e5e9a7b9
Prabin Poudel <15196941+coolprobn@users.noreply.github.com>
2024-06-25 10:05:46 +0545

    Fix the letter opener generator indentation issue

--- lib/generators/boring/letter_opener/install/install_generator.rb
+++ lib/generators/boring/letter_opener/install/install_generator.rb
@@ -8,9 +8,9 @@ module Boring
       def add_letter_opener_gem
         say "Adding letter_opener gem", :green
 
-        gem_content = <<~RUBY
-          \t# Preview email in the default browser instead of sending it to real mailbox
-          \tgem "letter_opener"
+        gem_content = <<~RUBY.indent(2)
+          \n# Preview email in the default browser instead of sending it to real mailbox
+          gem "letter_opener"
         RUBY
 
         insert_into_file "Gemfile", gem_content, after: /group :development do/
@@ -23,16 +23,15 @@ module Boring
       def configure_letter_opener
         say "Configuring letter_opener", :green
 
-        configuration_content = <<~RUBY.chomp
-          \n\t# Preview email in the browser instead of sending it
-          \tconfig.action_mailer.delivery_method = :letter_opener
-          \tconfig.action_mailer.perform_deliveries = true
-          end
+        configuration_content = <<~RUBY.chomp.indent(2)
+          \n# Preview email in the browser instead of sending it
+          config.action_mailer.delivery_method = :letter_opener
+          config.action_mailer.perform_deliveries = true
         RUBY
 
         gsub_file "config/environments/development.rb",
                   /end\Z/,
-                  configuration_content
+                  "#{configuration_content}\nend"
       end
     end
   end
